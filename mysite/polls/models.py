7ee4fa1c972c1c983220fac9dfe56c6cdd9ef41d
rhitabratakarar <kararrhitabrata@gmail.com>
2023-12-25 14:46:08 +0530

    added models and created migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """Model for a single poll Question. A question will have multiple choices."""
+
+    question_text: models.CharField = models.CharField(max_length=200)
+    pub_date: models.DateTimeField = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    """Model for single choice associated with a single question"""
+
+    question: models.ForeignKey = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text: models.CharField = models.CharField(max_length=200)
+    votes: models.IntegerField = models.IntegerField(default=0)
